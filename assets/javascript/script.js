4a47e4f59d203a9fe2078ffa57b36d2a06856b11
Michael Fisher <fisherdesign3@gmail.com>
2022-06-03 18:00:51 -0600

    The grab weather function keeps running before the longitude and latitude update. working on fixing that

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,29 +1,47 @@
-var lat = 0;
-var lon = 0;
-var place = 'Denver';
 
-console.log("test");
+var apiKey = '679579c74563b14f3d59e1876e9cfe37';
+var lata = 0;
+var long = 0;
+var city = 'Denver';
+
+
+grabLocation().then((grabWeather())).then((grabForcast));
+
 
 
 
 function grabLocation() {
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Host': 'forward-reverse-geocoding.p.rapidapi.com',
-            'X-RapidAPI-Key': 'f67cb71206mshc366dc7a6bc7cc6p1e7a9djsn309d42298c5c'
-        }
-    };
-    
-    fetch('https://forward-reverse-geocoding.p.rapidapi.com/v1/search?q=${place}&accept-language=en&polygon_threshold=0.0', options)
+fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`)
 	.then(function(response) {
             return response.json();
         })
     .then(function (data){
+        lata = data[0].lat;
+        long = data[0].lon;
+        console.log(lata);
+        console.log(long);
         console.log(data);
         })
 }
 
-/*function grabWeather()_{
-    console.log("Im here too!")
-}*/
+function grabWeather() {
+    console.log(lata);
+    console.log(long);
+    console.log(apiKey);
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lata}&lon=${long}&appid=${apiKey}`)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function (data){
+            console.log(data)
+        })
+    }
+function grabForcast() {
+        fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lata}&lon=${long}&appid=${apiKey}`)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function (data){
+            console.log(data)
+        })
+    }
